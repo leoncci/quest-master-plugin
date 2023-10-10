54fa689d0a3b10cab2a132bdcc3d27b96d18f590
Jonathan Somny <112839274+leoncci@users.noreply.github.com>
2023-10-10 14:53:44 +1100

    erreur base de donnée resolu

--- game/views/win.php
+++ game/views/win.php
@@ -5,20 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lose</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-        @font-face {
-            font-family: 'Gothic';
-            src: url('../public/assets/fonts/gothic.ttf') format('truetype');
-            /* You can also use 'woff' format for broader browser support */
-        }
+    @font-face {
+        font-family: 'Gothic';
+        src: url('../public/assets/fonts/gothic.ttf') format('truetype');
+        /* You can also use 'woff' format for broader browser support */
+    }
 
-        .title {
-            font-family: 'Gothic', sans-serif;
-            font-weight:900;
-            font-size: 70px;
-            /* Use 'Gothic' as the font-family name */
-        }
+    .title {
+        font-family: 'Gothic', sans-serif;
+        font-weight: 900;
+        font-size: 70px;
+        /* Use 'Gothic' as the font-family name */
+    }
     </style>
 </head>
 
@@ -67,43 +68,7 @@
                 </form>
             </div>
         </div>
-        <div class="col-md-3 pl-5 pr-5">
-            <table class="table border">
-                <thead class="thead-dark">
-                <tr>
-                    <th>Classement</th>
-                    <th>Username</th>
-                    <th>Score</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                global $wpdb;
-                $results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}questmaster ORDER BY score DESC LIMIT 10");
-                $position = 0;
-                foreach ($results as $row) {
-                    $position += 1;
-                    ?>
-                    <tr>
-                        <td><?php echo $position; ?></td>
-                        <td>
-                            <?php
-                            echo esc_html($row->username);
-                            ?>
-                        </td>
-                        <td>
-                            <?php
-                            echo esc_html($row->score);
-                            ?>
-                        </td>
 
-                    </tr>
-                    <?php
-                }
-                ?>
-                </tbody>
-            </table>
-        </div>
     </div>
 </body>
 
